7751e5be8c9c1b4a6fdf2ac253f19400c3058393
Dario <darionuberu@gmail.com>
2024-01-21 12:14:42 +0100

    feat: added hand movement

---
+++ scenes/character/hand.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+class_name Hand
+func _init():
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
+
+func _physics_process(delta):
+	velocity =  (get_global_mouse_position() - global_position)  * 8
+	move_and_slide()
+
+func _on_keyboard_area_body_entered(body):
+	print('area_entered')
+	$HandSprite.visible = true
+	$MouseSprite.visible = false
+
+
+func _on_keyboard_area_body_exited(body):
+	$HandSprite.visible = false
+	$MouseSprite.visible = true
