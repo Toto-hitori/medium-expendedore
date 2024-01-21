7a24c7c59f9cef71eaca7e0cd972f15976fa06fa
Dario <darionuberu@gmail.com>
2024-01-21 13:25:34 +0100

    feat: added emit of pressed button on keyboard

--- scenes/level/keyboard_key.gd
+++ scenes/level/keyboard_key.gd
@@ -1,7 +1,14 @@
 extends Node2D
 class_name KeyboardKey
+signal key_entered(val)
+signal key_left
 
 @export var val: int = 0 
 
 func _ready():
 	$Label.text = str(val)
+
+
+func _on_area_2d_body_entered(body):
+	key_entered.emit(val)
+
--- scenes/level/product_keyboad.gd
+++ scenes/level/product_keyboad.gd
@@ -2,11 +2,16 @@ extends Node2D
 signal keyboard_entered
 signal keyboard_exited
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+signal key_pressed(selected_key)
+var selected_key: int = 0
+func _ready():
+	for key in $Keys.get_children():
+		key.connect("key_entered",_on_key_entered)
+
 func _process(delta):
-	pass
-
-
+	if Input.is_action_just_pressed("click") and selected_key != 0:
+		key_pressed.emit(selected_key)
+	
 func _on_keyboard_area_body_entered(body):
 	if body is Hand:
 		keyboard_entered.emit()
@@ -14,3 +19,7 @@ func _on_keyboard_area_body_entered(body):
 func _on_keyboard_area_body_exited(body):
 	if body is Hand:
 		keyboard_exited.emit()
+		selected_key = 0
+
+func _on_key_entered(val):
+	selected_key = val
