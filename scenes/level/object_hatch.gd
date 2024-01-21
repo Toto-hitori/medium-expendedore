d1deb514a15bce783487b97889b745a00cf09403
Dario <darionuberu@gmail.com>
2024-01-22 00:26:43 +0100

    feat: updated stuff

--- scenes/level/object_hatch.gd
+++ scenes/level/object_hatch.gd
@@ -3,9 +3,16 @@ extends Node2D
 signal hand_entered
 signal hand_left
 
-func _on_object_area_body_entered(body):
+func _on_object_area_body_entered(_body):
 	hand_entered.emit()
 
-
-func _on_object_area_body_exited(body):
+func _on_object_area_body_exited(_body):
 	hand_left.emit()
+
+func _on_falling_item_detection_area_body_entered(body):
+	if(body is Item):
+		$ReadyProducts.add_child(body.scene_load.instantiate())
+		body.queue_free()
+		
+	
+
