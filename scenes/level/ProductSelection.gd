dd069ba23e36ee53879ebc7b6f9a914ae3a1a9af
Dario <darionuberu@gmail.com>
2024-01-21 19:37:15 +0100

    feat: updated product selection

--- scenes/level/ProductSelection.gd
+++ scenes/level/ProductSelection.gd
@@ -4,36 +4,46 @@ class_name ProductSelection
 @onready var timer = $CheckProductTimer
 var products : Array[Product]
 var current_selection : String = ""
-var selected_product: String   = ""
+var selected_product: Product 
 
+signal product_select(name)
 signal pick_product(val)
-
+signal invalid_product()
 func _init(): 
 	products = [Product.new('Cuadrado trufa',"11",1)]
 
 func add_key(val : int):
-	if(!product_selected()):
+	if(current_selection.length() < 2):
 		current_selection += str(val)
 		if(current_selection.length() == 2):
 			$ProductSelectionTimer.start()
 		
 func product_selected() -> bool:
-	return current_selection.length() == 2 && selected_product != ""
+	return current_selection.length() == 2 
 
 func get_product_with_code(code : String):
 	var p : Array[Product] = products.filter(func(product: Product):return product.code == current_selection)
-	return "Product not found" if p.size() == 0 else p[0].name
+	if p.size() == 0: 
+		return null
+	else: 
+		return p[0]
 
 func reset():
 	current_selection = ""
-	selected_product = ""
+	selected_product = null
 	print('selection reset')
-	print(product_selected())
+
+func ask_for_payment():
+	if(selected_product == null):
+		invalid_product.emit()
+		$ResetTimer.start()
+	else:
+		product_select.emit(selected_product.name)
+		print('buy product')
 
 func _on_product_selection_timer_timeout():
 	selected_product = get_product_with_code(current_selection)
-	$ResetTimer.start()
-	print('a')
+	ask_for_payment()
 
 func _on_reset_timer_timeout():
 	reset()
--- scenes/level/screen.gd
+++ scenes/level/screen.gd
@@ -10,11 +10,18 @@ func _process(_delta):
 	if(!products.product_selected()):
 		if(products.current_selection != ""):
 			set_text(products.current_selection)
-	else:
-		set_text(products.selected_product)
+		else:
+			set_text("Selecciona un producto")
+	
 
 func set_text(text : String):
 	$Label.text = text
 	
 func _on_product_keyboad_key_pressed(selected_key):
 	products.add_key(selected_key)
+
+func _on_products_selection_invalid_product():
+	set_text("Producto invalido")
+
+func _on_products_selection_product_select(name):
+	set_text(name)
