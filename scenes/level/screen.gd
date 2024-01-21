87149f5feb3eff90ed3af55c76d378defc4bf6d8
Dario <darionuberu@gmail.com>
2024-01-21 13:46:03 +0100

    feat: added product name showcase

--- scenes/level/screen.gd
+++ scenes/level/screen.gd
@@ -7,8 +7,14 @@ func _init():
 	products = ProductSelection.new()
 
 func _process(delta):
-	if(products.current_selection != ""):
-		$Label.text = products.current_selection
+	if(!products.product_selected):
+		if(products.current_selection != ""):
+			set_text(products.current_selection)
+	else:
+		set_text(products.selected_product)
+
+func set_text(text : String):
+	$Label.text = text
 	
 func _on_product_keyboad_key_pressed(selected_key):
 	products.add_key(selected_key)
