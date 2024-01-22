eb8f1be4af01839a65ac36ac796a8aca86c83b88
Dario <darionuberu@gmail.com>
2024-01-22 18:01:13 +0100

    feat: moved hand sprite

--- scenes/character/hand.gd
+++ scenes/character/hand.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 class_name Hand
+var ready_to_grab : bool = false
+signal grab_object
 func _init():
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
 
 func _physics_process(_delta):
 	velocity =  (get_global_mouse_position() - global_position)  * 8
 	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("click") and ready_to_grab:
+		grab_object.emit()
 
 func _on_product_keyboad_keyboard_entered():
 	$HandSprite.frame = 0
@@ -22,8 +28,10 @@ func _on_object_hatch_hand_entered():
 	$HandSprite.frame = 1
 	$HandSprite.visible = true
 	$MouseSprite.visible = false
+	ready_to_grab = true
 
 
 func _on_object_hatch_hand_left():
 	$HandSprite.visible = false
 	$MouseSprite.visible = true
+	ready_to_grab = false
