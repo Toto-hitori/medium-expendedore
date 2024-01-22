210eb43e96ccb74fda24880c37a17f93caf2537f
Dario <darionuberu@gmail.com>
2024-01-22 01:09:20 +0100

    git: last commit for tonight

--- scenes/level/object_hatch.gd
+++ scenes/level/object_hatch.gd
@@ -12,7 +12,7 @@ func _on_object_area_body_exited(_body):
 func _on_falling_item_detection_area_body_entered(body):
 	if(body is Item):
 		var item = body.scene_load.instantiate()
-		item.rotation = 3.14/2
+		item.rotation = 3.14/3
 		$ReadyProducts.add_child(item)
 		body.queue_free()
 		
