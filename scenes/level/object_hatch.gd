3e076ea4ee0611552986407d3ab19cd5b63dbaa2
Dario <darionuberu@gmail.com>
2024-01-22 18:01:03 +0100

    feat: added product grabbing

--- scenes/level/object_hatch.gd
+++ scenes/level/object_hatch.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 signal hand_entered
 signal hand_left
+@onready var ready_products = $ReadyProducts
+func remove_first_object():
+	if(ready_products.get_child_count() > 0):
+		ready_products.get_child(0).queue_free()
 
 func _on_object_area_body_entered(_body):
 	hand_entered.emit()
@@ -13,7 +17,8 @@ func _on_falling_item_detection_area_body_entered(body):
 	if(body is Item):
 		var item = body.scene_load.instantiate()
 		item.rotation = 3.14/3
-		$ReadyProducts.add_child(item)
+		ready_products.add_child(item)
 		body.queue_free()
-	
 
+func _on_hand_grab_object():
+	remove_first_object()
