d5ec5ea98e82ca9298ecc7829e533bd2b95dcc77
Dario <darionuberu@gmail.com>
2024-01-23 00:49:13 +0100

    feat: added rotation

--- scenes/level/product_stands.gd
+++ scenes/level/product_stands.gd
@@ -4,11 +4,11 @@ var products = ["res://scenes/items/cuadrado_trufa.tscn"]
 
 func _on_screen_release_product(code : String):
 	release_product(code)
-	print(code)
 
 func release_product(code: String):
 	var roller = $RollyThings.get_child(int(code[0])-1).get_child(int(code[1])-1)
 	roller.roll()
 	var item = load(products[int(code[0])-1]).instantiate()
 	item.global_position = roller.position 
+	item.rotation = 3.14/6
 	$FallingProducts.add_child(item)
