a3be4f72174559603565efa017e1771e7bb50495
Dario <darionuberu@gmail.com>
2024-01-23 02:08:05 +0100

    feat: updated position of spawning

--- scenes/level/objects/product_stands.gd
+++ scenes/level/objects/product_stands.gd
@@ -7,6 +7,6 @@ func release_product(code: String):
 	var roller = $RollyThings.get_child(int(code[0])-1).get_child(int(code[1])-1)
 	roller.roll()
 	var item = Products.release_product(code)
-	item.global_position = roller.position 
+	item.global_position = roller.global_position - Vector2(130,140)
 	item.rotation = 3.14/6
 	$FallingProducts.add_child(item)
